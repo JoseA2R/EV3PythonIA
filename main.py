6be0d4848097295e276d939cc4bedeadb21a0b64
JoseA2R <joseagustindrf@gmail.com>
2019-12-10 21:16:58 +0000

    eliminar parados, colocar 2 andar tras em cada cor e criacao da funcao colocarPecas

--- main.py
+++ main.py
@@ -14,15 +14,8 @@ tank_drive = MoveTank(OUTPUT_B, OUTPUT_C)
 def main():
     abrir()
     testDistance_inicio()
-    andar()
-    direita()
-    andar()
-    andar()
-    andar()
-    esquerda()
-    testDistance()
-    andar_tras_inicio()
-    testDistance()
+    while(col.color() != 1):
+        colocarPecas()
     """
     abrir()
     testDistance()
@@ -31,6 +24,17 @@ def main():
     direita()
 """
 
+def colocarPecas():
+    andar()
+    direita()
+    andar()
+    andar()
+    andar()
+    esquerda()
+    testDistance()
+    andar_tras_inicio()
+    
+
 def testColor_inicio():
     
     x = True
@@ -38,16 +42,11 @@ def testColor_inicio():
         if(col.color() == 7):   #ccor castanho
             fechar()
             sleep(1)
-            #parado()
             direita()
-            #parado()
             andar()
-            #parado()
             abrir()
             sleep(1)
-            #parado()
             andar_tras()
-            #parado()
             esquerda()
             testDistance_inicio()
             x = False
@@ -55,16 +54,11 @@ def testColor_inicio():
         if(col.color() == 5):   #cor vermelha
             fechar()
             sleep(1)
-            #parado()
             direita()
-            #parado()
             andar()
-            #parado()
             abrir()
             sleep(1)
-            #parado()
             andar_tras()
-            #parado()
             esquerda()
             testDistance_inicio()
             x = False
@@ -72,16 +66,11 @@ def testColor_inicio():
         if(col.color() == 4):   #cor amarelo
             fechar()
             sleep(1)
-            #parado()
             direita()
-            #parado()
             andar()
-            #parado()
             abrir()
             sleep(1)
-            #parado()
             andar_tras()
-            #parado()
             esquerda()
             testDistance_inicio()
             x = False
@@ -89,16 +78,11 @@ def testColor_inicio():
         if(col.color() == 3):   #cor verde
             fechar()
             sleep(1)
-            #parado()
             direita()
-            #parado()
             andar()
-            #parado()
             abrir()
             sleep(1)
-            #parado()
             andar_tras()
-            #parado()
             esquerda()
             testDistance_inicio()
             x = False
@@ -106,16 +90,11 @@ def testColor_inicio():
         if(col.color() == 2):   #cor azul
             fechar()
             sleep(1)
-            #parado()
             direita()
-            #parado()
             andar()
-            #parado()
             abrir()
             sleep(1)
-            #parado()
             andar_tras()
-            #parado()
             esquerda()
             testDistance_inicio()
             x = False
@@ -136,94 +115,77 @@ def testColor():
     x = True
     while(x):
         if(col.color() == 7):
-            #fechar()
             fechar()
             sleep(1)
-            #parado()
             andar_tras()
             andar_tras()
-            #parado()
             direita()
             andar_tras()
+            andar_tras()
+            andar_tras()
             esquerda()
-            #parado()
             andar_tras_inicio()
             andar33()
             abrir()
             sleep(1)
-            #parado()
             andar_tras33()
             andar_tras_inicio()
-            testDistance()
             x = False
         print(col.color())
         if(col.color() == 5):
-            #fechar()
             fechar()
             sleep(1)
-            #parado()
             andar_tras()
             andar_tras()
-            #parado()
             direita()
             andar_tras()
+            andar_tras()
+            andar_tras()
             esquerda()
-            #parado()
             andar_tras_inicio()
             andar44()
-            #parado()
+            abrir()
             abrir()
             sleep(1)
-            #parado()
             andar_tras44()
             andar_tras_inicio()
-            testDistance()
             x = False
         print(col.color())
         if(col.color() == 4):
-            #fechar()
             fechar()
             sleep(1)
-            #parado()
             andar_tras()
             andar_tras()
-            #parado()
             direita()
             andar_tras()
+            andar_tras()
+            andar_tras()
             esquerda()
-            #parado()
             andar_tras_inicio()
             andar43()
+            abrir()
             abrir()
             parado()
             andar_tras43()
             andar_tras_inicio()
-            testDistance()
             x = False
         print(col.color())
         if(col.color() == 3):
-            #fechar()
             fechar()
             sleep(1)
-            #parado()
             andar_tras()
             andar_tras()
-            #parado()
             direita()
             andar_tras()
+            andar_tras()
+            andar_tras()
             esquerda()
-            #parado()
             andar_tras_inicio()
             andar34()
-            #parado()
-            #parado()
             abrir()
             abrir()
-            #parado()
-            #parado()
             andar_tras34()
             andar_tras_inicio()
-            testDistance()
             x = False
         print(col.color())
         if(col.color() == 2):
@@ -236,6 +198,8 @@ def testColor():
             #parado()
             direita()
             andar_tras()
+            andar_tras()
+            andar_tras()
             esquerda()
             #parado()
             andar_tras_inicio()
@@ -245,7 +209,6 @@ def testColor():
             #parado()
             andar_tras24()
             andar_tras_inicio()
-            testDistance()
             x = False
         print(col.color())
         if(col.color() == 1):
@@ -255,16 +218,16 @@ def testColor():
 
 def testDistance_inicio():
 
-    ola = 16
-    print(ola)
-    while(ola>15 ):
-        ola=int(us.distance(False))/10
-        print(ola)
+    distancia = 16
+    print(distancia)
+    while(distancia > 15 ):
+        distancia=int(us.distance(False))/10
+        print(distancia)
         andar()
-    if(ola<15):
+    if(distancia<15):
         testColor_inicio()
 
-    return ola
+    return distancia
 
 def testDistance():
 
